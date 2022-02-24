accd274fc9835fe564365641a80e6f99d1b770ed
John Wason <wason@wasontech.com>
2022-02-24 03:49:09 -0500

    local win building

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,14 @@
 @echo off
 
-md build
-cd build
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake -GNinja ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
-  ..
+  -S ros_industrial_cmake_boilerplate ^
+  -B build_dir
 if %errorlevel% neq 0 exit /b %errorlevel%
-cmake --build . --config Release -j 4
+cmake --build build_dir --config Release -j 4
 if %errorlevel% neq 0 exit /b %errorlevel%
-cmake --build . --config Release --target install
+cmake --build build_dir --config Release --target install
 if %errorlevel% neq 0 exit /b %errorlevel%
--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,8 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
   -DCMAKE_PREFIX_PATH:PATH=$PREFIX \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DCMAKE_INSTALL_LIBDIR=lib \
-  ..
+  -S ros_industrial_cmake_boilerplate \
+  -B build_dir
 
 cmake --build . --config Release -- -j$CPU_COUNT
 cmake --build . --config Release --target install
