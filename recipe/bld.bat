84b07f6f4429f2f9d5e82bc260a3a065ce697dd0
John Wason <wason@wasontech.com>
2022-03-10 17:26:44 -0500

    Use src dir

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,7 +4,7 @@ cmake -GNinja ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
-  -S ros_industrial_cmake_boilerplate ^
+  -S src/ros_industrial_cmake_boilerplate ^
   -B build_dir
 if %errorlevel% neq 0 exit /b %errorlevel%
 cmake --build build_dir --config Release -j 4
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX \
   -DCMAKE_PREFIX_PATH:PATH=$PREFIX \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DCMAKE_INSTALL_LIBDIR=lib \
-  -S ros_industrial_cmake_boilerplate \
+  -S src/ros_industrial_cmake_boilerplate \
   -B build_dir
 
 cmake --build build_dir --config Release -- -j$CPU_COUNT
