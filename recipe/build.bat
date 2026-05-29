a790dd1dfdb7e6e3c069118b7913249e9609245c
John Wason <wason@wasontech.com>
2026-05-29 17:33:50 -0400

    Improve build

--- recipe/build.bat
+++ recipe/build.bat
@@ -2,13 +2,14 @@
 
 if exist src rmdir /s /q src
 mkdir src
-echo */ci/* > tar_excludes.txt
+echo */ci/*> tar_excludes.txt
 tar xf source.tar.gz --strip-components=1 -C src -X tar_excludes.txt
 
 cmake -GNinja ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
   -DCMAKE_BUILD_TYPE:STRING=Release ^
+  -DCMAKE_VERBOSE_MAKEFILE=ON ^
   -S src ^
   -B build_dir
 if %errorlevel% neq 0 exit /b %errorlevel%
